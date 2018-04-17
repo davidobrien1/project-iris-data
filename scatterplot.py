9dcb5fe20c5d4e2289d9a9abe92b9ed73ce8ec3b
David O'Brien <davidobrien1@outlook.com>
2018-04-17 23:01:11 +0100

    Created scatterplot in scatterplot.py

--- scatterplot.py
+++ scatterplot.py
@@ -9,13 +9,13 @@ import numpy
 data = numpy.genfromtxt('data/iris.csv', delimiter=',')   # https://stackoverflow.com/questions/3518778/how-to-read-csv-into-record-array-in-numpy
 
 sepallength = data[:,0]
-sepalwidth = 
-petallength =
-Petalwidth = 
+sepalwidth = data[:,1]
+petallength = data[:,2]
+Petalwidth = data[:,3]
 
 
 #import the matplotlib package
 
 import matplotlib.pyplot as pl
-pl.hist(firstcol)
+pl.scatter(sepallength, sepalwidth)
 pl.show()
