98eefabcb16a42cbb336089553d9fd779322be3f
David O'Brien <davidobrien1@outlook.com>
2018-04-19 22:09:57 +0100

    Created 2 scatter plots

--- scatterplot.py
+++ scatterplot.py
@@ -6,7 +6,15 @@ import numpy
 
 # read data file into array.
 
-sepallength, sepalwidth, petallength, petalwidth, species = numpy.genfromtxt('data/iris.csv', delimiter=',',usecols=0,1,2,3,4)   # https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.genfromtxt.html
+data = numpy.genfromtxt('data/iris.csv', delimiter=',')   # https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.genfromtxt.html
+
+
+sepallength = data[:,0]      # https://stackoverflow.com/questions/3518778/how-to-read-csv-into-record-array-in-numpy - this lists all numbers in the first column of data
+sepalwidth = data[:,1]
+petallength = data[:,2]
+petalwidth = data[:,3]
+
+
 
 # import the matplotlib.pyplot package
 
@@ -15,4 +23,10 @@ import matplotlib.pyplot as pl
 # create scatter plot of sepal length vs sepal width for all specie types
 
 pl.scatter(sepallength, sepalwidth)
+pl.show()
+
+# create scatter plot of petal length vs petal width for all specie types
+
+pl.scatter(petallength, petalwidth)
+pl.show()
 
