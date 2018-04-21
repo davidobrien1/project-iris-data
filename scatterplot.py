656754f587c820d5b9982348fbeebe4c8250d366
David O'Brien <davidobrien1@outlook.com>
2018-04-21 10:01:44 +0100

    Scatter plot with different colors for species types solved!!!

--- scatterplot.py
+++ scatterplot.py
@@ -4,17 +4,22 @@
 
 import numpy
 
-# read data file into array.
+# read data file into array.  Using numpy.genfromtext, i was able to load the code numpy.genfromtxt('data/iris.csv', delimiter=',') 
+# and this was fine for creating the basic scatter plots, but when it came to creating different colour data points
+# for the different species, it was not picking up the 5th column and converting it to color
+# I received the 'nan' error regularly which means not a number.  I know I needed to Python to read this column as a string.
+# To investigate this I started looking through the different parameters of numpy.genfromtext at https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.genfromtxt.html
+# I found the 'usecols' and 'dtype' parameters and was able to use this to set the data types for the first 4 columns and then the 5th column
 
-data = numpy.genfromtxt('data/iris.csv', delimiter=',')   # https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.genfromtxt.html
+data = numpy.genfromtxt('data/iris.csv', delimiter=',', usecols=(0, 1, 2, 3), dtype=float)   # https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.genfromtxt.html
 
 
 sepallength = data[:,0]      # https://stackoverflow.com/questions/3518778/how-to-read-csv-into-record-array-in-numpy - this lists all numbers in the first column of data
 sepalwidth = data[:,1]
 petallength = data[:,2]
 petalwidth = data[:,3]
-species = str(data[:,4])
 
+species = numpy.genfromtxt('data/iris.csv', delimiter=',', usecols=(4), dtype=str)
 
 
 # import the matplotlib.pyplot package
@@ -35,11 +40,8 @@ pl.show()
 
 #setting colors for each flower type
 
-import pandas as pd
+color_dict = { 'Iris-setosa':'red', 'Iris-versicolor':'blue', 'Iris-virginica':'green'}
 
-df = pd.DataFrame(dict(A=sepallength, B=sepalwidth, C=species))
+pl.scatter(sepallength, sepalwidth, color=[ color_dict[i] for i in species ] )
 
-colors = {'Iris-versicolor': 'red', 'Iris-setosa': 'green','Iris-virginica': 'blue'}
-
-pl.scatter(df.A, df.B, c=df.C.map(colors))
 pl.show()
