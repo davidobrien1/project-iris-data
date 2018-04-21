efecc4853c112d3a3f9b2208dc0fb1510a7f6b19
David O'Brien <davidobrien1@outlook.com>
2018-04-21 22:06:08 +0100

    Added labels to histograms and saved new images

--- pihist.py
+++ pihist.py
@@ -21,13 +21,25 @@ fourthcol = data[:,3]
 
 import matplotlib.pyplot as pl  # taken from 'Matplotlib Pyplot' video tutorial topic 8
 pl.hist(firstcol)               # plots a histogram of the first column of data. taken from 'Matplotlib Pyplot' video tutorial topic 8
+pl.title('Histogram of Sepal Length')      # Went to the following link to find the '.title' parameter to create a title for the scatter plot dhttps://matplotlib.org/api/pyplot_api.html
+pl.xlabel('Centimetres') # Went to the following link to find the '.xlabel' parameter to create a title for the scatter plot dhttps://matplotlib.org/api/pyplot_api.html
+pl.ylabel('Quantity')  # Went to the following link to find the '.ylabel' parameter to create a title for the scatter plot dhttps://matplotlib.org/api/pyplot_api.html
 pl.show()                       # shows the last histogram.  taken from 'Matplotlib Pyplot' video tutorial topic 8
 
 pl.hist(secondcol)               # plots a histogram of the first column of data. taken from 'Matplotlib Pyplot' video tutorial topic 8
+pl.title('Histogram of Sepal Width')      # Went to the following link to find the '.title' parameter to create a title for the scatter plot dhttps://matplotlib.org/api/pyplot_api.html
+pl.xlabel('Centimetres') # Went to the following link to find the '.xlabel' parameter to create a title for the scatter plot dhttps://matplotlib.org/api/pyplot_api.html
+pl.ylabel('Quantity')  # Went to the following link to find the '.ylabel' parameter to create a title for the scatter plot dhttps://matplotlib.org/api/pyplot_api.html
 pl.show()                       # shows the last histogram.  taken from 'Matplotlib Pyplot' video tutorial topic 8
 
 pl.hist(thirdcol)               # plots a histogram of the first column of data. taken from 'Matplotlib Pyplot' video tutorial topic 8
+pl.title('Histogram of Petal Length')      # Went to the following link to find the '.title' parameter to create a title for the scatter plot dhttps://matplotlib.org/api/pyplot_api.html
+pl.xlabel('Centimetres') # Went to the following link to find the '.xlabel' parameter to create a title for the scatter plot dhttps://matplotlib.org/api/pyplot_api.html
+pl.ylabel('Quantity')  # Went to the following link to find the '.ylabel' parameter to create a title for the scatter plot dhttps://matplotlib.org/api/pyplot_api.html
 pl.show()                       # shows the last histogram.  taken from 'Matplotlib Pyplot' video tutorial topic 8
 
 pl.hist(fourthcol)               # plots a histogram of the first column of data. taken from 'Matplotlib Pyplot' video tutorial topic 8
+pl.title('Histogram of Petal Width')      # Went to the following link to find the '.title' parameter to create a title for the scatter plot dhttps://matplotlib.org/api/pyplot_api.html
+pl.xlabel('Centimetres') # Went to the following link to find the '.xlabel' parameter to create a title for the scatter plot dhttps://matplotlib.org/api/pyplot_api.html
+pl.ylabel('Quantity')  # Went to the following link to find the '.ylabel' parameter to create a title for the scatter plot dhttps://matplotlib.org/api/pyplot_api.html
 pl.show()                       # shows the last histogram.  taken from 'Matplotlib Pyplot' video tutorial topic 8
